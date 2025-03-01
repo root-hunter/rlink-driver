1f1ddf38c5081b8576f0aee0f5f95ff8af51861a
roothunter <antonio.ricciardi99@outlook.it>
2025-03-01 18:03:06 +0100

    testing and refactoring

--- src/linux/screen.rs
+++ src/linux/screen.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 use std::env;
 use std::str::FromStr;
 
-use gstreamer::prelude::GstBinExtManual;
+use gstreamer::prelude::{GObjectExtManualGst, GstBinExtManual};
 use tokio::sync::mpsc::{self, Receiver, Sender};
 use tokio::task::{self, JoinHandle};
 
@@ -108,21 +108,20 @@ impl Screen {
         }
     }
 
-    fn get_caps_str(config: ScreenConfig) -> String {
-        return format!(
+    fn get_appsrc_caps(config: ScreenConfig) -> gstreamer::Caps {
+        let caps_str = format!(
             "video/x-raw,format={},width={},height={},framerate={}/1",
             config.format, config.width, config.height, config.framerate
         );
+        let caps_str = caps_str.as_str();
+        return gstreamer::Caps::from_str(caps_str).unwrap();
     }
 
-    fn setup<F>(&mut self, config: ScreenConfig, setup_pipeline: F)
-    where 
-        F: Fn() -> gstreamer::Element
+    fn setup<F>(&mut self, setup_pipeline: F)
+    where
+        F: Fn() -> gstreamer::Element,
     {
-        let (
-            tx,
-            mut rx
-        ) = mpsc::channel::<Frame>(10);
+        let (tx, mut rx) = mpsc::channel::<Frame>(10);
         let mut frame_count = 0;
         let appsrc = setup_pipeline();
 
@@ -143,26 +142,114 @@ impl Screen {
     }
 
     fn setup_xvimagesink(&mut self, config: ScreenConfig) {
-        self.setup(config.clone(), || {
-            let caps_str = Screen::get_caps_str(config.clone());
-            let caps = Caps::from_str(caps_str.as_str()).unwrap();
+        self.setup(|| {
+            let caps = Screen::get_appsrc_caps(config.clone());
 
             let pipeline = Pipeline::new();
-            let appsrc = ElementFactory::make("appsrc").build().unwrap();
+            let appsrc = ElementFactory::make("appsrc")
+                .property("caps", caps)
+                .property("is-live", true)
+                .build()
+                .unwrap();
+
             let sink = ElementFactory::make("xvimagesink").build().unwrap();
 
-            appsrc.set_property("caps", caps);
-            appsrc.set_property("is-live", true);
-            appsrc.set_property("do-timestamp", true);
-
-            pipeline.add_many(&[
-                &appsrc,
-                &sink
-            ]).unwrap();
+            pipeline.add_many(&[&appsrc, &sink]).unwrap();
 
             appsrc.link(&sink).unwrap();
             pipeline.set_state(gstreamer::State::Playing).unwrap();
-            
+
+            return appsrc;
+        });
+    }
+
+    fn setup_xvimagesink_compressed(&mut self, config: ScreenConfig) {
+        self.setup(|| {
+            let pipeline = Pipeline::new();
+
+            let appsrc = ElementFactory::make("appsrc").build().unwrap();
+            let queue = ElementFactory::make("queue").build().unwrap();
+            let x264enc = ElementFactory::make("x264enc").build().unwrap();
+            let h264parse = ElementFactory::make("h264parse").build().unwrap();
+            let avdec_h264 = ElementFactory::make("avdec_h264").build().unwrap();
+            let videoconvert1 = ElementFactory::make("videoconvert").build().unwrap();
+            let videoconvert2 = ElementFactory::make("videoconvert").build().unwrap();
+            let autovideosink = ElementFactory::make("autovideosink").build().unwrap();
+
+            let appsrc_caps =
+                Caps::from_str(&"video/x-raw,format=BGRx,width=1920,height=1080,framerate=60/1")
+                    .unwrap();
+
+            appsrc.set_property("caps", appsrc_caps);
+            appsrc.set_property("is-live", true);
+            appsrc.set_property("do-timestamp", true);
+
+            // Aggiungi gli elementi alla pipeline
+            pipeline
+                .add_many(&[
+                    &appsrc,
+                    &queue,
+                    &videoconvert1,
+                    &x264enc,
+                    &h264parse,
+                    &avdec_h264,
+                    &videoconvert2,
+                    &autovideosink,
+                ])
+                .unwrap();
+
+            // Collega gli elementi
+            appsrc.link(&queue).unwrap();
+            queue.link(&videoconvert1).unwrap();
+            videoconvert1.link(&x264enc).unwrap();
+            x264enc.link(&h264parse).unwrap();
+            h264parse.link(&avdec_h264).unwrap();
+            avdec_h264.link(&videoconvert2).unwrap();
+            videoconvert2.link(&autovideosink).unwrap();
+
+            // Imposta lo stato della pipeline su Playing
+            pipeline.set_state(gstreamer::State::Playing).unwrap();
+
+            return appsrc;
+        });
+    }
+
+    fn setup_udpsink(&mut self, config: ScreenConfig) {
+        self.setup(|| {
+            let caps_str = "format=BGRx";
+            let caps = Caps::from_str(caps_str).unwrap();
+
+            let pipeline = Pipeline::new();
+            let appsrc = ElementFactory::make("appsrc").build().unwrap();
+            let queue = ElementFactory::make("queue").build().unwrap();
+            let videoconvert = ElementFactory::make("videoconvert").build().unwrap();
+            let encoder = gstreamer::ElementFactory::make("x264enc").build().unwrap();
+            let queue2 = ElementFactory::make("queue").build().unwrap();
+            let sink = ElementFactory::make("autovideosink").build().unwrap();
+
+            appsrc.set_property("caps", caps);
+            //appsrc.set_property("is-live", true);
+            //appsrc.set_property("do-timestamp", true);
+
+            pipeline
+                .add_many(&[
+                    &appsrc,
+                    &queue,
+                    &videoconvert, // Aggiungi l'encoder
+                    &encoder,      // Aggiungi l'encoder
+                    &queue2,       // Aggiungi l'encoder
+                    &sink,
+                ])
+                .unwrap();
+
+            appsrc.link(&queue).unwrap();
+            queue.link(&videoconvert).unwrap(); // Collega a encoder
+            videoconvert.link(&encoder).unwrap(); // Collega a encoder
+            encoder.link(&queue2).unwrap(); // Collega il sink all'encoder
+            queue2.link(&sink).unwrap(); // Collega il sink all'encoder
+
+            pipeline.set_state(gstreamer::State::Playing).unwrap();
+
             return appsrc;
         });
     }
@@ -170,6 +257,7 @@ impl Screen {
     fn init_channel(&mut self) {
         let config = self.config.clone();
 
+        //self.setup_compressed_xvimagesink(config);
         self.setup_xvimagesink(config);
     }
 }
